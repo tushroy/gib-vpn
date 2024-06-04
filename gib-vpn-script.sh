93dbb12089dbfd46d551251232648225403f115a
Tushar <skydel.net@gmail.com>
2024-06-04 21:24:25 +0600

    updt

--- gib-vpn-script.sh
+++ gib-vpn-script.sh
@@ -6,7 +6,7 @@ set username $env(GIB_VPN_USERNAME)
 set password $env(GIB_VPN_PASSWORD)
 set vpnserver $env(GIB_VPN_SERVER)
 
-spawn openconnect --protocol=gp --os=win --mtu=1300 --csd-wrapper=./hipreport.sh --script=./vpnc-script.sh "$vpnserver"
+spawn openconnect --protocol=gp --os=win --mtu=1300 --deflate --compression=all --force-dpd=500 --csd-wrapper=./hipreport.sh --script=./vpnc-script.sh "$vpnserver"
 
 expect "Enter 'yes' to accept, 'no' to abort; anything else to view:"
 send -- "yes\r"
